d90b30b66edc77fd04b9de8559a7f0a4eb96a402
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-03-25 22:42:06 -0500

    Interlude changes for registration, email permissions and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,14 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: exception.message
   end
 
+  def after_sign_in_path_for(resource)
+    topics_path
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name << :avatar
+    devise_parameter_sanitizer.for(:sign_up) << :name << :avatar << :email_favorites
   end
 
 end
