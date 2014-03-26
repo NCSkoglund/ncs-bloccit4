7776a9b0648d27c81a77dceb772677f64889a2a9
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-03-26 13:14:20 -0500

    Created user profile

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
+  default_scope { order('created_at DESC')}
   scope :visible_to, ->(user) { user ? scoped : where(public: true) }
 end
