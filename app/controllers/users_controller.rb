1650a460232ab2fe3708356a59c12dee6bbdb7cc
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-03-29 20:41:50 -0500

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: :index
+
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
 
   def show
     @user = User.find(params[:id])
