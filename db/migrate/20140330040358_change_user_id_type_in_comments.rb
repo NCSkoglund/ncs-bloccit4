5ce95a02c4b9024c8677852200306c07e77d3c5f
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-03-30 13:02:51 -0500

    User_id type migration adjustment

--- db/migrate/20140330040358_change_user_id_type_in_comments.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserIdTypeInComments < ActiveRecord::Migration
-  def up
-    change_column :comments, :user_id, :integer
-  end
-
-  def down
-    change_column :comments, :user_id, :string
-  end
-end
